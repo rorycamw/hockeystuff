3f26c935bc0f8269ce6be401148d2c20db4112db
gparale <32000524+gparale@users.noreply.github.com>
2018-11-27 19:07:19 -0800

    Update StandingsComponent.js
    Made some comment changes

--- hockey_stuff/view/StandingsComponent.js
+++ hockey_stuff/view/StandingsComponent.js
@@ -19,6 +19,7 @@ export default class StandingsComponent extends Component {
 	}
 
 	getFavTeams() {
+		//Change to database
 		return(['Vancouver Canucks', 'Florida Panthers', 'Vegas Golden Knights']);
 	}
 
